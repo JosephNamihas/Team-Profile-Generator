0856610dc98c524741e75d92fdfad3bdfa3b39b9
Joseph Namihas (Admin) <jnamihasadmin@bsuh.nhs.uk>
2023-02-24 19:24:56 +0000

    Started Methods

--- starter/lib/Engineer.js
+++ starter/lib/Engineer.js
@@ -1,1 +1,11 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+class Engineer extends Employee {
+    constructor(gitHub){
+        this.gitHub = gitHub;
+    }
+
+    getGitHub() {};
+    getRole() {}; // Overide to return Engineer
+};
+
+module.exports = {Engineer};
--- starter/lib/Intern.js
+++ starter/lib/Intern.js
@@ -1,1 +1,11 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+class Intern extends Employee {
+    constructor(school) {
+        this.school = school;
+    }
+
+    getSchool() {};
+    getRole() {}; // Return Intern
+};
+
+module.exports = {Intern}; 
